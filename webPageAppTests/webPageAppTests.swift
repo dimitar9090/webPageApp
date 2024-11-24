4e2f957f3b9c90c0f276dc97197cda1fd288ff99
Dimitar Anguelov <d.angelov.9090@gmail.com>
2024-11-25 00:51:58 +0200

    Initial Commit

---
+++ webPageAppTests/webPageAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  webPageAppTests.swift
+//  webPageAppTests
+//
+//  Created by Dimitar Angelov on 25.11.24.
+//
+
+import Testing
+@testable import webPageApp
+
+struct webPageAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

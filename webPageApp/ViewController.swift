326766bb19e456badd4dc2d8579d3fa46fe2b939
Dimitar Anguelov <d.angelov.9090@gmail.com>
2024-11-25 00:57:02 +0200

    To convert your website into an iOS app with a single WebView, here’s a straightforward approach using Swift and UIKit. This app will display your website without integrating any native features, which makes the process simpler.

--- webPageApp/ViewController.swift
+++ webPageApp/ViewController.swift
@@ -5,15 +5,26 @@
 //  Created by Dimitar Angelov on 25.11.24.
 //
 
-import UIKit
 
-class ViewController: UIViewController {
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+
+    var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Create a WebView and set it as the main view
+        webView = WKWebView(frame: self.view.frame)
+        webView.navigationDelegate = self
+        self.view.addSubview(webView)
+        
+        // Load your website URL
+        if let url = URL(string: "https://angelovtech.com") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
-
-
 }
-
